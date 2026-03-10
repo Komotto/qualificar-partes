1ff2f8a9d9e21712da33209ebedcfa7d93e343c8
Komotto <58824478+Komotto@users.noreply.github.com>
2026-03-10 11:24:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -28,10 +28,32 @@ def executar():
     open(LOCK_FILE, "w").close()
 
     try:
+        result = subprocess.run(
+            ["bash", ROOT_SCRIPT],
+            cwd=WORK_DIR,
+            capture_output=True,
+            text=True
+        )
+
+        return {
+            "status": "Processo finalizado",
+            "stdout": result.stdout,
+            "stderr": result.stderr
+        }
+
+    except subprocess.CalledProcessError as e:
+        return {"erro": str(e)}
+
+    finally:
+        if os.path.exists(LOCK_FILE):
+            os.remove(LOCK_FILE)
+    try:
         subprocess.run(
             ["bash", ROOT_SCRIPT],
+            cwd=WORK_DIR,
             check=True,
-            cwd=WORK_DIR
+            capture_output=True,
+            text=True
         )
     except subprocess.CalledProcessError as e:
         return {"erro": str(e)}
